80af2eb8057e38eb54e31af32aa6c7d49d556b48
bartoszkubica <bartoszkubica70@gmail.com>
2022-06-13 18:21:50 +0200

    test

--- src/Tests/DotBootstrap.Persistence.IntegrationTests/DbContextProvider.cs
+++ src/Tests/DotBootstrap.Persistence.IntegrationTests/DbContextProvider.cs
@@ -10,7 +10,7 @@ public static class DbContextProvider
     {
         services.ConfigureDbContext<TestDbContext>(x =>
         {
-            x.UseNpgsql("User ID=user;Password=user;Host=localhost;Port=5432;Database=test-db;");
+            x.UseNpgsql("User ID=user;Password=user;Host=postgres;Port=5432;Database=test-db;");
         });
 
     }
